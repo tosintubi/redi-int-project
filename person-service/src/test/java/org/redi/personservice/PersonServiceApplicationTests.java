01a83f184940c92834740e4eb48da1446a18cfa6
tosintubi <1065402+tosintubi@users.noreply.github.com>
2021-06-03 01:18:02 +0200

    Initial commit - Person service

---
+++ person-service/src/test/java/org/redi/personservice/PersonServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.redi.personservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
