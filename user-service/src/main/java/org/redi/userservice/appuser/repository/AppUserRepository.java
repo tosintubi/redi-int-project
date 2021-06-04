9babc112b3608e74defab20eae515bf124163747
tosintubi <1065402+tosintubi@users.noreply.github.com>
2021-06-04 18:10:58 +0200

    Appliction UserRepo, used email as username

---
+++ user-service/src/main/java/org/redi/userservice/appuser/repository/AppUserRepository.java
@@ -0,0 +1,14 @@
+package org.redi.userservice.appuser.repository;
+
+import org.redi.userservice.appuser.model.AppUser;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Repository
+@Transactional(readOnly = true)
+public interface AppUserRepository {
+
+    Optional<AppUser> findByEmail(String email);
+}
