c2baaa19a98fcd6dde48597ad833b89615d698a5
tosintubi <1065402+tosintubi@users.noreply.github.com>
2021-06-04 22:33:53 +0200

    implemented tokenConfirmation

--- user-service/src/main/java/org/redi/userservice/registration/controller/RegistrationController.java
+++ user-service/src/main/java/org/redi/userservice/registration/controller/RegistrationController.java
@@ -3,10 +3,7 @@ package org.redi.userservice.registration.controller;
 import lombok.AllArgsConstructor;
 import org.redi.userservice.registration.model.RegistrationRequest;
 import org.redi.userservice.registration.service.RegistrationService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1/registration")
@@ -15,8 +12,13 @@ public class RegistrationController {
 
     private final RegistrationService registrationService;
 
-    @PostMapping("/add/")
+    @PostMapping("/add")
     public String register (@RequestBody RegistrationRequest request){
        return registrationService.register(request);
+    }
+
+    @GetMapping(path = "/confirm")
+    public String confirm(@RequestParam("token") String token) {
+        return registrationService.confirmToken(token);
     }
 }
