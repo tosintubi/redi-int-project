af9dd46216c200d589539ca7e21862e7b2066dca
tosintubi <1065402+tosintubi@users.noreply.github.com>
2021-06-04 18:32:12 +0200

    some more refactoring

--- user-service/src/main/java/org/redi/userservice/appuser/service/AppUserService.java
+++ user-service/src/main/java/org/redi/userservice/appuser/service/AppUserService.java
@@ -1,15 +1,24 @@
 package org.redi.userservice.appuser.service;
 
+import lombok.AllArgsConstructor;
+import org.redi.userservice.appuser.repository.AppUserRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
+
 public class AppUserService implements UserDetailsService {
 
+    private final static  String USER_NOT_FOUND_MESSAGE = "User with email %s not found";
+    private  final AppUserRepository appUserRepository;
+
     @Override
-    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
-        return null;
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return appUserRepository.findByEmail(email).orElseThrow(() -> new UsernameNotFoundException(
+                String.format(USER_NOT_FOUND_MESSAGE, email)
+        ));
     }
 }
