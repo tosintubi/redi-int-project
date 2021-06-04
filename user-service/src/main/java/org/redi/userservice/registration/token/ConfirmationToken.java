004a05067d29d09a177cf319456dde6bcd0b2fb5
tosintubi <1065402+tosintubi@users.noreply.github.com>
2021-06-04 21:03:55 +0200

    confirmation Token model

---
+++ user-service/src/main/java/org/redi/userservice/registration/token/ConfirmationToken.java
@@ -0,0 +1,52 @@
+package org.redi.userservice.registration.token;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.redi.userservice.appuser.model.AppUser;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+
+@NoArgsConstructor
+public class ConfirmationToken {
+
+    @Id
+    @SequenceGenerator(
+            name = "token_sequence",
+            sequenceName = "confirmation_token_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "confirmation_token_sequence"
+    )
+    private Long id;
+
+    @Column(nullable = false)
+    private String token;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(nullable = false)
+    private LocalDateTime expiresAt;
+    private LocalDateTime confirmedAt;
+
+    @OneToOne
+    @JoinColumn(nullable = false, name = "app_user_id")
+    private AppUser appUser;
+
+    public ConfirmationToken(String token, LocalDateTime createdAt,
+                             LocalDateTime expiresAt, LocalDateTime confirmedAt) {
+        this.token = token;
+        this.createdAt = createdAt;
+        this.expiresAt = expiresAt;
+        this.confirmedAt = confirmedAt;
+    }
+}
