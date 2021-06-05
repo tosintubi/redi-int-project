25a2a676073d0110b52f38f9e47a257df1b16e0a
tosintubi <1065402+tosintubi@users.noreply.github.com>
2021-06-05 13:18:18 +0200

    EmailSenderService

--- user-service/src/main/java/org/redi/userservice/email/EmailSenderService.java
+++ user-service/src/main/java/org/redi/userservice/email/EmailSenderService.java
@@ -1,17 +1,42 @@
 package org.redi.userservice.email;
 
 import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
 
 
 @Service
 @AllArgsConstructor
 public class EmailSenderService implements EmailSender {
 
-    // private final JavaMailSender mailSender;
+    private final JavaMailSender mailSender;
+    private final static Logger LOGGER = LoggerFactory.getLogger((EmailSenderService.class));
 
     @Override
-    public void sendRegistrationEmail(String to, String email) {
+    @Async // Async Method
+    public void sendRegistrationEmail(String recipientEmail, String emailMessage) {
          // TODO: use java mail sender
+
+
+        try {
+
+            MimeMessage mimeMessage = mailSender.createMimeMessage();
+            MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, "utf-8");
+            mimeMessageHelper.setText(emailMessage, true);
+            mimeMessageHelper.setTo(recipientEmail);
+            mimeMessageHelper.setSubject("Confirm your email");
+            mimeMessageHelper.setFrom("hello@redibooks.org");
+            mailSender.send(mimeMessage);
+        } catch (MessagingException exception){
+            LOGGER.error("Error sending Email: ", exception);
+            throw new IllegalStateException(" Failed to send registration email");
+        }
     }
 }
