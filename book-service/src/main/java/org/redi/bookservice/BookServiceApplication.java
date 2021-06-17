c0652a424b81628b41dd250c6f7cf117081cd129
Tosin <tosintubi@users.noreply.github.com>
2021-06-17 23:31:32 +0100

    pom.xml dependencies

--- book-service/src/main/java/org/redi/bookservice/BookServiceApplication.java
+++ book-service/src/main/java/org/redi/bookservice/BookServiceApplication.java
@@ -3,7 +3,6 @@ package org.redi.bookservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
 public class BookServiceApplication {
 
 	public static void main(String[] args) {
