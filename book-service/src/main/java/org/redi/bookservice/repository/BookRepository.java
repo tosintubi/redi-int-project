fc8026593d3791c05bb2e8654ab93490a55778c1
Tosin <tosintubi@users.noreply.github.com>
2021-06-28 00:41:09 +0200

    findBookByTitle()

--- book-service/src/main/java/org/redi/bookservice/repository/BookRepository.java
+++ book-service/src/main/java/org/redi/bookservice/repository/BookRepository.java
@@ -4,7 +4,9 @@ package org.redi.bookservice.repository;
 import org.redi.bookservice.model.Author;
 import org.redi.bookservice.model.Book;
 import org.redi.bookservice.model.BookCatalogue;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -14,4 +16,8 @@ public interface BookRepository  extends JpaRepository<Book, Long> {
 
     List<Book> findBookByAuthors(String author);
     Book findByIsbn(String isbn);
+
+
+    @Query(value = "SELECT b from Book b WHERE CONCAT(b.title, b.originalTitle) LIKE %?1% ")
+    List<Book> findBookByTitleOrOriginalTitle(String search);
 }
