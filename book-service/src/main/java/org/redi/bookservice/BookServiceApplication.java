0b7b0e8d3e17a9b84d9fd84c90f29796131f52ce
Tosin <tosintubi@users.noreply.github.com>
2021-06-27 15:23:08 +0200

    findByIsbn

--- book-service/src/main/java/org/redi/bookservice/BookServiceApplication.java
+++ book-service/src/main/java/org/redi/bookservice/BookServiceApplication.java
@@ -3,6 +3,7 @@ package org.redi.bookservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class BookServiceApplication {
 
 	public static void main(String[] args) {
--- book-service/src/main/java/org/redi/bookservice/repository/BookRepository.java
+++ book-service/src/main/java/org/redi/bookservice/repository/BookRepository.java
@@ -10,6 +10,7 @@ import java.util.List;
 
 @Repository
 public interface BookRepository  extends JpaRepository<Book, Long> {
-//    List<Book> findAllByISBN(String isbn);
-//    List<Book> findBookByAuthors(Author author);
+    /*List<Book> findAllByISBN(String isbn);
+    List<Book> findBookByAuthors(Author author);*/
+    Book findByIsbn(String isbn);
 }
