364bc4be29a151fbac8e52517d955591d761018c
Tosin <tosintubi@users.noreply.github.com>
2021-06-30 01:27:29 +0200

    fixed error due to columnName

--- book-service/src/main/java/org/redi/bookservice/model/Book.java
+++ book-service/src/main/java/org/redi/bookservice/model/Book.java
@@ -21,14 +21,28 @@ public class Book {
     private String isbn;
     private String isbn13;
     private String title;
+    @Column(name = "originaltitle")
     private String originalTitle;
 
     //    @ManyToMany()
     //    private List<Author> authors;
     private String authors;
+
+    @Column(name = "languagecode")
     private String languageCode;
 
+    @Column(name = "publicationyear")
     private Integer publicationYear;
+
+    @Column(name = "publisher")
     private String publisher;
+
+    @Column(name = "category")
     private String category;
+
+    @Column(name = "imageurl")
+    private String imageUrl;
+
+    @Column(name = "smallimageurl")
+    private String smallImageUrl;
 }
